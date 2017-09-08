122ea2cb68e82baeafea9aa485ebc93bdf25cb05
Sam Ekong <SamEkong@gmail.com>
2017-09-08 18:55:10 -0500

    added multipoint routes

--- mapController.js
+++ mapController.js
@@ -1,24 +1,62 @@
+var directionsDisplay;
+var directionsService;
+var map;
+
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  directionsService = new google.maps.DirectionsService();
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
     center: {lat: 30.211537759527246, lng: -92.02144145965576}
   });
 
   map.addListener('click', function(e) {
 	addLocation(e.latLng);
-    placeMarkerAndPanTo(e.latLng, map);
+    placeMarkerAndPanTo(e.latLng);
   });
+  directionsDisplay.setMap(map);
 }
 
 function addLocation(latLng) {
   var div = document.getElementById('locations');
-  div.innerHTML += '<div class="col-sm-6"><p class="location">{lat: '+latLng.lat()+', lng: '+latLng.lng()+'}</p></div><div class="col-sm-6"><button type="button"><span class="glyphicon glyphicon-trash"></span></button></div>';
-}
+  div.innerHTML += 
+  '<li class="location" class="list-group-item" data-loc='+JSON.stringify(latLng)+'>'+latLng+'\
+  </li>';
 
-function placeMarkerAndPanTo(latLng, map) {
+  /*div.innerHTML += 
+  '<li class="location" class="list-group-item" data-loc='+JSON.stringify(latLng)+'>'+latLng+'\
+  <button type="button"><span class="glyphicon glyphicon-trash"></span></button></li>';
+	*/
+  }
+
+function placeMarkerAndPanTo(latLng) {
   var marker = new google.maps.Marker({
     position: latLng,
     map: map
   });
   map.panTo(latLng);
+}
+
+function getWayPoints(e){
+	return {location: JSON.parse(e.dataset.loc), stopover: true };
+}
+
+function calcRoute() {
+  var locations = document.getElementsByClassName("location");
+  var wayPoints = Array.prototype.slice.call( locations ).map(getWayPoints);
+  var start = JSON.parse(locations[0].dataset.loc);
+  var end = JSON.parse(locations[1].dataset.loc);
+  var request = {
+    origin: start,
+    destination: start,
+    travelMode: 'DRIVING',
+	waypoints: wayPoints.slice(1),
+	optimizeWaypoints: true
+  };
+  
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+    }
+  });
 }
